775c4c6801b2c8d3f552b798174d5c8015786a17
pkubiak <solmyrmag@gmail.com>
2014-02-10 00:44:42 +0100

    Completed implementation of single thread CannyEdgeDetector.

--- src/lib/singleton/edge_detectors.cpp
+++ src/lib/singleton/edge_detectors.cpp
@@ -3,6 +3,9 @@
 #include<functional>
 #include "edge_detectors.hpp"
 #include "../common/fs.hpp"
+#include<queue>
+#include<cstring>
+
 #define _DEBUG 1
 
 namespace shaper{
@@ -13,7 +16,7 @@ void CannyEdgeDetector::apply(std::array<float*, 1> INs, std::array<float**, 1> 
 	float *out = NULL;
 	
 	//Blue image to reduce noises
-	shaper::singleton::filters::GaussianBlurFilter blurFilter(5, 1.1);
+	shaper::singleton::filters::GaussianBlurFilter blurFilter(10, 2.0);
 	blurFilter.apply({INs[0]}, {&out}, w, h);
 
 	 
@@ -31,23 +34,16 @@ void CannyEdgeDetector::apply(std::array<float*, 1> INs, std::array<float**, 1> 
 	});
 	
 	//Detect angle
-	if(false){
+	/*if(false){
 		float *gradAngle = NULL;
 		shaper::singleton::filters::utils::applyPerPixelFilter({gradX, gradY}, &gradAngle, w, h, [](float x, float y) -> float{
 			return atan2f(x,y);
 		});
 		
-	}
-	shaper::singleton::filters::normalize(gradMagnitude, w, h);
+	}*/
+	//shaper::singleton::filters::normalize(gradMagnitude, w, h);
 	
-	//calculate direction
-	/*shaper::singleton::filters::utils::applyPerPixelFilter({gradX, gradY}, &gradDirection, w, h, [](float x, float y) -> float{
-		if( (x > 0.3742*y) == (x < -0.3742*y)) return 0.0;//kierunek |
-		if( (y > 0.3742*x) == (y < -0.3742*x)) return 1.0;//kierunek -
-		if( x*y > 0 )return 2.0;//kierunek /
-		return 3.0;//kierunek \. 
-	});*/
-	
+	//nonmaximal supression
 	shaper::singleton::filters::utils::applyPerPixelFilter(&gradDirection, w, h, [gradX, gradY, gradMagnitude, w,h](int x, int y) -> float{
 		float mag = gradMagnitude[x+y*w], X = gradX[x+y*w], Y = gradY[x+y*w];
 		
@@ -70,7 +66,9 @@ void CannyEdgeDetector::apply(std::array<float*, 1> INs, std::array<float**, 1> 
 			return mag;
 		return 0.0f;
 	});
-
+	
+	shaper::singleton::filters::normalize(gradDirection, w, h);
+	
 	#ifdef _DEBUG
 	shaper::singleton::filters::normalize(INs[0], w, h);
 	shaper::fs::save("canny_input.pgm", "PGM", {INs[0]}, w, h);
@@ -83,18 +81,59 @@ void CannyEdgeDetector::apply(std::array<float*, 1> INs, std::array<float**, 1> 
 	shaper::singleton::filters::normalize(gradMagnitude, w, h);
 	shaper::fs::save("canny_magnitude.pgm", "PGM", {gradMagnitude}, w, h);
 	//shaper::fs::save("canny_angle.pgm", "PGM", {grad}, w, h);
-	shaper::singleton::filters::normalize(gradDirection, w, h);
+	//shaper::singleton::filters::normalize(gradDirection, w, h);
 	shaper::fs::save("canny_direction.pgm", "PGM", {gradDirection}, w, h);
 	#endif
-	//nonmaximal supression
+	
+	//tresholding
+	
+	float lowThreshold = 0.05;
+	float highThreshold = 0.2;
+	
+	memset(out, 0, sizeof(float)*w*h);
+	std::queue<int > Q;
+	for(int y=0;y<h;y++)
+		for(int x=0;x<w;x++){
+			if(gradDirection[x+y*w]>highThreshold){
+				int pos = x+y*w, nextpos = -1;
+				gradDirection[pos] = 0.0f;	
+				while(true){
+					nextpos = -1;
+					
+					out[pos] = 1.0f;
+					for(int dy=(pos>w?-1:0);dy<=(pos<w*(h-1)?1:0);dy++)
+						for(int dx=(pos%w?-1:0);dx<=((pos%w)==w-1?0:1);dx++)
+							if((dx||dy)&&gradDirection[pos+dx+dy*w]>lowThreshold){
+								gradDirection[pos+dx+dy*w] = 0.0f;
+								if(nextpos == -1)
+									nextpos = pos+dx+dy*w;
+								else Q.push(pos+dx+dy*w);
+							}
+					
+					if(nextpos==-1){
+						if(Q.empty())
+							break;
+						pos = Q.front();
+						Q.pop();
+					}else
+						pos = nextpos;
+				}
+			}
+		}
+	
+	#ifdef _DEBUG
+	shaper::singleton::filters::normalize(out, w, h);
+	shaper::fs::save("canny_threshold.pgm", "PGM", {out}, w, h);
+	//shaper::singleton::filters::normalize(gradDirection, w, h);
+	shaper::fs::save("canny_direction_remained.pgm", "PGM", {gradDirection}, w, h);
+	#endif
+	
+	
 	if(OUTs[0] != NULL)
-		*OUTs[0] = gradDirection;
-	
-	
-	//thresholding
-	
+		*OUTs[0] = out;
+
 	//free memory
-	free(out); free(gradX); free(gradY); free(gradMagnitude); 
+	 free(gradX); free(gradY); free(gradMagnitude); 
 }
 
 
